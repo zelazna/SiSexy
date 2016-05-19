536aec590d41897bfd34b11ab384749dcdc7c4ca
zelazna <constantin.guidon@gmail.com>
2016-05-19 22:29:35 +0200

    3D ok

--- scripts/main.js
+++ scripts/main.js
@@ -171,7 +171,25 @@ $(document).ready(function () {
     });
 
     //Effet Switch de formulaires
-    
+    var init = function () {
+        var carousel = document.getElementById('carousel'),
+            navButtons = document.querySelectorAll('#navigation button'),
+            panelCount = carousel.children.length,
+            transformProp = Modernizr.prefixed('transform'),
+            theta = 0,
+
+            onNavButtonClick = function (event) {
+                var increment = parseInt(event.target.getAttribute('data-increment'));
+                theta += ( 180 / panelCount ) * increment * -1;
+                carousel.style[transformProp] = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
+            };
+
+        for (var i = 0; i < 2; i++) {
+            navButtons[i].addEventListener('click', onNavButtonClick, false);
+        }
+
+    };
+    window.addEventListener('DOMContentLoaded', init, false);
 });
 
 
